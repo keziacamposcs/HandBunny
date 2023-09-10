954172b058cb922780c25c7c6579d25545c3e5e6
Kezia Campos <kezia.campos@icloud.com>
2023-09-10 11:52:39 -0300

    up

--- js/slide/slide copy.js
+++ js/slide/slide copy.js
@@ -6,9 +6,6 @@ const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const canvasCtx = canvasElement.getContext('2d');
 const fingerDirectionElement = document.getElementById('finger-direction');
 let canClick = true;
-const ZOOM_IN_THRESHOLD = 0.1; // Defina conforme necessário
-const ZOOM_OUT_THRESHOLD = -0.1; // Defina conforme necessário
-let currentScale = 1;  // Escala inicial
 
 // =========================
 // Módulo de Detecção de Gestos
@@ -23,7 +20,7 @@ const GestureModule = (() => {
             for (const landmarks of results.multiHandLandmarks) {
                 drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, {color: '#F1FAEE', lineWidth: 3});
                 drawLandmarks(canvasCtx, landmarks, {color: '#E63946', lineWidth: 1});
-                handleGestureDirection(checkFingerDirection(landmarks), landmarks);
+                handleGestureDirection(checkFingerDirection(landmarks));
             }
         }
         canvasCtx.restore();
@@ -35,12 +32,12 @@ const GestureModule = (() => {
         return indexPos.x > thumbPos.x ? "Direita" : "Esquerda";
     }
 
-    function handleGestureDirection(direction, handLandmarks) {
+    function handleGestureDirection(direction) {
         fingerDirectionElement.textContent = direction;
         setTimeout(() => {
             fingerDirectionElement.textContent = "";
         }, 2000);
-    
+
         if (direction === "Direita" && canClick) {
             SlideModule.nextSlide();
             canClick = false;
@@ -48,19 +45,11 @@ const GestureModule = (() => {
             SlideModule.prevSlide();
             canClick = false;
         }
-    
-        const zoomChange = checkZoomGesture(handLandmarks);
-        if (zoomChange > ZOOM_IN_THRESHOLD) {
-            SlideModule.zoomIn();
-        } else if (zoomChange < ZOOM_OUT_THRESHOLD) {
-            SlideModule.zoomOut();
-        }
-    
+
         setTimeout(() => {
             canClick = true;
         }, 5000);
     }
-    
 
     return {
         init: () => {
@@ -107,55 +96,19 @@ const SlideModule = (() => {
                 queueRenderPage(pageNum);
             }
         },
-
         prevSlide: () => {
             if (pageNum > 1) {
                 pageNum--;
                 queueRenderPage(pageNum);
             }
         },
-
-        zoomIn: () => {
-            currentScale += 0.1;  // Ajuste conforme necessário
-            renderPage(pageNum);
-        },
-    
-        zoomOut: () => {
-            currentScale -= 0.1;  // Ajuste conforme necessário
-            renderPage(pageNum);
-        },
-
         init: () => {
             document.getElementById('inputGroupFile').addEventListener('change', function() { /*...*/ });
             document.getElementById('prev').addEventListener('click', SlideModule.prevSlide);
             document.getElementById('next').addEventListener('click', SlideModule.nextSlide);
-            document.getElementById('zoomIn').addEventListener('click', SlideModule.zoomIn);
-            document.getElementById('zoomOut').addEventListener('click', SlideModule.zoomOut);            
         }
     };
 })();
-
-// =========================
-// Módulo de Zoom
-// =========================
-function checkZoomGesture(handLandmarks) {
-    const thumbPos = handLandmarks[4];
-    const indexPos = handLandmarks[8];
-    const distance = Math.sqrt(Math.pow((thumbPos.x - indexPos.x), 2) + Math.pow((thumbPos.y - indexPos.y), 2));
-
-    // Compare com a distância anterior (se disponível)
-    if (typeof checkZoomGesture.previousDistance === "undefined") {
-        checkZoomGesture.previousDistance = distance;
-        return 0; 
-    }
-
-    const distanceChange = distance - checkZoomGesture.previousDistance;
-    checkZoomGesture.previousDistance = distance;
-    return distanceChange;
-}
-
-
-
 
 // =========================
 // Inicialização dos Módulos
