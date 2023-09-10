96a657ed453f74702b1ec68c29a92462c7f9bd98
Kezia Campos <kezia.campos@icloud.com>
2023-09-10 13:06:58 -0300

    Update slide.js

--- js/slide/slide.js
+++ js/slide/slide.js
@@ -1,10 +1,64 @@
-// Importar os módulos necessários do Mediapipe
-import { Hands } from "@mediapipe/hands";
-import { Camera } from "@mediapipe/camera_utils";
-import { drawConnectors, drawLandmarks, HAND_CONNECTIONS } from "@mediapipe/drawing_utils";
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
 
-// Configuração do detector de mãos do MediaPipe Hands
-const hands = new Hands({ locateFile: (file) => {
+// Função que processa os resultados da detecção de mãos
+function onResults(results)
+{
+    canvasCtx.save();
+    // Limpa o canvas
+    canvasCtx.clearRect( 0, 0, canvasElement.width, canvasElement.height );
+
+    // Desenha a imagem da câmera no canvas
+    canvasCtx.drawImage( results.image, 0, 0, canvasElement.width, canvasElement.height );
+    if (results.multiHandLandmarks)
+    {
+        for (const landmarks of results.multiHandLandmarks)
+        {
+            drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,  {color: '#F1FAEE', lineWidth: 3}  );
+            drawLandmarks(canvasCtx, landmarks, {color: '#E63946', lineWidth: 1});
+
+            // verifica a direção do dedo indicador
+            const direction = checkFingerDirection(landmarks);
+
+            // Atualiza a legenda com a direção do dedo indicador
+            //fingerDirectionElement.textContent = checkFingerDirection(landmarks);
+            fingerDirectionElement.textContent = direction;
+        }
+    }
+    canvasCtx.restore();
+}
+
+function checkFingerDirection(handLandmarks)
+{
+    const thumbPos = handLandmarks[4];
+    const indexPos = handLandmarks[8];
+
+    if (indexPos.x > thumbPos.x)
+    {
+        // Exibe "Direita" por 1 segundos
+        document.getElementById("finger-direction").textContent = "Direita";
+        setTimeout(() => {
+            document.getElementById("finger-direction").textContent = "";
+        }, 2000);
+        return "Direita";
+    }
+    else
+    {
+        // Exibe "Esquerda" por 1 segundos
+        document.getElementById("finger-direction").textContent = "Esquerda";
+        setTimeout(() => {
+            document.getElementById("finger-direction").textContent = "";
+        }, 2000);
+        return "Esquerda";
+    }
+}
+
+const fingerDirectionElement = document.getElementById('finger-direction');
+// Fim - Função que verifica a direção do dedo indicador
+
+// Configura o detector de mãos do MediaPipe Hands
+const hands = new Hands({locateFile: (file) => {
   return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
 }});
 
@@ -15,70 +69,18 @@ hands.setOptions({
   minDetectionConfidence: 0.5,
   minTrackingConfidence: 0.5
 });
+hands.onResults(onResults);
 
 // Inicializa a câmera
-const videoElement = document.querySelector('.input_video');
-const canvasElement = document.querySelector('.output_canvas');
-const canvasCtx = canvasElement.getContext('2d');
-
-const camera = new Camera(videoElement, {
+const camera = new Camera(videoElement,
+{
   onFrame: async () => {
-    await hands.send({ image: videoElement });
+    await hands.send({image: videoElement});
   },
   width: 1024,
   height: 768
 });
-
 camera.start();
-
-// Função que processa os resultados da detecção de mãos
-function onResults(results) {
-  canvasCtx.save();
-  // Limpa o canvas
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-
-  // Desenha a imagem da câmera no canvas
-  canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-  if (results.multiHandLandmarks) {
-    for (const landmarks of results.multiHandLandmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#F1FAEE', lineWidth: 3 });
-      drawLandmarks(canvasCtx, landmarks, { color: '#E63946', lineWidth: 1 });
-
-      // verifica a direção do dedo indicador
-      const direction = checkFingerDirection(landmarks);
-
-      // Atualiza a legenda com a direção do dedo indicador
-      fingerDirectionElement.textContent = direction;
-    }
-  }
-  canvasCtx.restore();
-}
-
-function checkFingerDirection(handLandmarks) {
-  const thumbPos = handLandmarks[4];
-  const indexPos = handLandmarks[8];
-
-  if (indexPos.x > thumbPos.x) {
-    // Exibe "Direita" por 1 segundos
-    document.getElementById("finger-direction").textContent = "Direita";
-    setTimeout(() => {
-      document.getElementById("finger-direction").textContent = "";
-    }, 2000);
-    return "Direita";
-  } else {
-    // Exibe "Esquerda" por 1 segundos
-    document.getElementById("finger-direction").textContent = "Esquerda";
-    setTimeout(() => {
-      document.getElementById("finger-direction").textContent = "";
-    }, 2000);
-    return "Esquerda";
-  }
-}
-
-const fingerDirectionElement = document.getElementById('finger-direction');
-
-// Configura o detector de mãos do MediaPipe Hands
-hands.onResults(onResults);
 
 //PDF
 var pdfjsLib = window['pdfjs-dist/build/pdf'];
@@ -93,22 +95,26 @@ canvas = document.getElementById('the-canvas'),
 ctx = canvas.getContext('2d');
 
 // renderiniza a página
-function renderPage(num) {
+function renderPage(num)
+{
   pageRendering = true;
   // Using promise to fetch the page
-  pdfDoc.getPage(num).then(function(page) {
-    var viewport = page.getViewport({ scale: scale });
+  pdfDoc.getPage(num).then(function(page)
+  {
+    var viewport = page.getViewport({scale: scale});
     canvas.height = viewport.height;
     canvas.width = viewport.width;
 
     // Render PDF page into canvas context
-    var renderContext = { canvasContext: ctx, viewport: viewport };
+    var renderContext = {canvasContext: ctx, viewport: viewport };
     var renderTask = page.render(renderContext);
 
     // Wait for rendering to finish
-    renderTask.promise.then(function() {
+    renderTask.promise.then(function()
+    {
       pageRendering = false;
-      if (pageNumPending !== null) {
+      if (pageNumPending !== null)
+      {
         renderPage(pageNumPending);
         pageNumPending = null;
       }
@@ -117,43 +123,54 @@ function renderPage(num) {
   document.getElementById('page_num').textContent = num;
 }
 
-function queueRenderPage(num) {
-  if (pageRendering) {
+function queueRenderPage(num)
+{
+  if (pageRendering)
+  {
     pageNumPending = num;
-  } else {
+  }
+  else
+  {
     renderPage(num);
   }
 }
 
 // Função que muda para a página anterior
-function onPrevPage() {
-  if (pageNum <= 1) {
+function onPrevPage()
+{
+  if (pageNum <= 1)
+  {
     return;
   }
   pageNum--;
   queueRenderPage(pageNum);
 }
-
 document.getElementById('prev').addEventListener('click', onPrevPage);
+// Fim - Função que muda para a página anterior
 
-// Função que muda para a próxima página
-function onNextPage() {
-  if (pageNum >= pdfDoc.numPages) {
+// Função que muda para a proxima página
+function onNextPage()
+{
+  if (pageNum >= pdfDoc.numPages) 
+  {
     return;
   }
   pageNum++;
   queueRenderPage(pageNum);
 }
-
 document.getElementById('next').addEventListener('click', onNextPage);
+// Fim - Função que muda para a proxima página
 
-// Carrega o slide
-document.getElementById('inputGroupFile').addEventListener('change', function() {
+//Carrega o slide
+document.getElementById('inputGroupFile').addEventListener('change', function()
+{
   var file = this.files[0];
   var fileReader = new FileReader();
-  fileReader.onload = function() {
+  fileReader.onload = function()
+  {
     var typedarray = new Uint8Array(this.result);
-    pdfjsLib.getDocument(typedarray).promise.then(function(pdfDoc_) {
+    pdfjsLib.getDocument(typedarray).promise.then(function(pdfDoc_)
+    {
       pdfDoc = pdfDoc_;
       document.getElementById('page_count').textContent = pdfDoc.numPages;
       renderPage(pageNum);
@@ -162,30 +179,33 @@ document.getElementById('inputGroupFile').addEventListener('change', function() 
   fileReader.readAsArrayBuffer(file);
 });
 
+
+
 const botao_prox = document.getElementById("next");
 const botao_antes = document.getElementById("prev");
 
 let canClick = true; // Variável para controlar o delay entre cliques
 
-const observer = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    const texto = mutation.target.textContent.toLowerCase();
-    if (texto.includes("direita") && canClick) {
-      botao_prox.click();
-      canClick = false;
-      setTimeout(() => {
-        canClick = true;
-      }, 5000); // Delay 
-      break;
-    } else if (texto.includes("esquerda") && canClick) {
-      botao_antes.click();
-      canClick = false;
-      setTimeout(() => {
-        canClick = true;
-      }, 5000); // Delay 
-      break;
+   const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+        const texto = mutation.target.textContent.toLowerCase();
+        if (texto.includes("direita") && canClick) {
+            botao_prox.click();
+            canClick = false;
+            setTimeout(() => {
+                canClick = true;
+            }, 5000); // Delay 
+            break;
+        }
+        else if (texto.includes("esquerda") && canClick) {
+            botao_antes.click();
+            canClick = false;
+            setTimeout(() => {
+                canClick = true;
+            }, 5000); // Delay 
+            break;
+        }
     }
-  }
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
