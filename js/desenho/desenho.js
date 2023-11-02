2ddb45568828355f8b2ad08e3679359443646328
Kezia Campos <kezia.campos@icloud.com>
2023-11-01 22:28:12 -0300

    Update desenho.js

--- js/desenho/desenho.js
+++ js/desenho/desenho.js
@@ -1,173 +1,116 @@
-// Módulo para a detecção de mãos
-const HandDetectionModule = (function () {
+(function () {
+    // ----- DOM Elements -----
     const videoElement = document.getElementsByClassName('input_video')[0];
     const canvasElement = document.getElementsByClassName('output_canvas')[0];
     const canvasCtx = canvasElement.getContext('2d');
-    const fingerDirectionElement = document.getElementById('finger-direction');
+    const canvasQuadro = document.getElementById('quadro');
+    const ctx = canvasQuadro.getContext('2d');
   
-    // Função que processa os resultados da detecção de mãos
-    function onResults(results) {
-      canvasCtx.save();
-      // Limpa o canvas
-      canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    // ----- State -----
+    let desenhando = false;
+    let x, y;
   
-      // Desenha a imagem da câmera no canvas
-      canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+    // ----- Drawing Functions -----
+    function drawHandLandmarks(results, ctx) {
       if (results.multiHandLandmarks) {
         for (const landmarks of results.multiHandLandmarks) {
-          drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS, { color: '#F1FAEE', lineWidth: 3 });
-          drawLandmarks(canvasCtx, landmarks, { color: '#E63946', lineWidth: 1 });
+          const landmark = landmarks[4];
+          ctx.beginPath();
+          ctx.arc(landmark.x * canvasQuadro.width, landmark.y * canvasQuadro.height, 5, 0, 2 * Math.PI);
+          ctx.fillStyle = '#1D3557';
+          ctx.fill();
+        }
+      }
+    }
   
-          // verifica a direção do dedo indicador
-          const direction = checkFingerDirection(landmarks);
+    function clearCanvas(canvas, context) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
   
-          // Atualiza a legenda com a direção do dedo indicador
-          fingerDirectionElement.textContent = direction;
+    // ----- Hand Tracking -----
+    function onResults(results) {
+      canvasCtx.save();
+      clearCanvas(canvasElement, canvasCtx);
+      canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+  
+      if (results.multiHandLandmarks) {
+        for (const landmarks of results.multiHandLandmarks) {
+          drawLandmarks(canvasCtx, [landmarks[8]], { color: '#E63946', lineWidth: 1 });
+          const indexFinger = landmarks[8];
+          const handOpen = indexFinger.y < landmarks[5].y;
+  
+          if (handOpen) {
+            ctx.strokeStyle = '#1D3557';
+            ctx.lineWidth = 5;
+            ctx.lineCap = 'round';
+          } else {
+            ctx.strokeStyle = '#f5f5f5';
+            ctx.lineWidth = 5;
+            ctx.lineCap = 'round'; 
+          }
+  
+          ctx.beginPath();
+          ctx.moveTo(indexFinger.x * canvasQuadro.width, indexFinger.y * canvasQuadro.height);
+          ctx.lineTo(landmarks[7].x * canvasQuadro.width, landmarks[7].y * canvasQuadro.height);
+          ctx.stroke();
         }
       }
       canvasCtx.restore();
     }
   
-    function checkFingerDirection(handLandmarks) {
-      const thumbPos = handLandmarks[4];
-      const indexPos = handLandmarks[8];
-  
-      if (indexPos.x > thumbPos.x) {
-        // Exibe "Direita" por 1 segundos
-        document.getElementById("finger-direction").textContent = "Direita";
-        setTimeout(() => {
-          document.getElementById("finger-direction").textContent = "";
-        }, 2000);
-        return "Direita";
-      } else {
-        // Exibe "Esquerda" por 1 segundos
-        document.getElementById("finger-direction").textContent = "Esquerda";
-        setTimeout(() => {
-          document.getElementById("finger-direction").textContent = "";
-        }, 2000);
-        return "Esquerda";
-      }
-    }
-  
-    return {
-      onResults,
-    };
-  })();
-  
-  // Módulo para a detecção de PDF
-  const PdfDetectionModule = (function () {
-    var pdfjsLib = window['pdfjs-dist/build/pdf'];
-    pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-  
-    var pdfDoc = null,
-      pageNum = 1,
-      pageRendering = false,
-      pageNumPending = null,
-      scale = 0.8,
-      canvas = document.getElementById('the-canvas'),
-      ctx = canvas.getContext('2d');
-  
-    // renderiniza a página
-    function renderPage(num) {
-      pageRendering = true;
-      // Using promise to fetch the page
-      pdfDoc.getPage(num).then(function (page) {
-        var viewport = page.getViewport({ scale: scale });
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-  
-        // Render PDF page into canvas context
-        var renderContext = { canvasContext: ctx, viewport: viewport };
-        var renderTask = page.render(renderContext);
-  
-        // Wait for rendering to finish
-        renderTask.promise.then(function () {
-          pageRendering = false;
-          if (pageNumPending !== null) {
-            renderPage(pageNumPending);
-            pageNumPending = null;
-          }
-        });
+    // ----- Event Listeners -----
+    function setupEventListeners() {
+      canvasQuadro.addEventListener("mouseup", function(evt) {
+        desenhando = true;
+        x = evt.clientX - canvasQuadro.getBoundingClientRect().left;
+        y = evt.clientY - canvasQuadro.getBoundingClientRect().top;
       });
-      document.getElementById('page_num').textContent = num;
+  
+      canvasQuadro.addEventListener("mousemove", function(evt) {
+        if (desenhando) {
+          ctx.beginPath();
+          ctx.moveTo(x, y);
+          x = evt.clientX - canvasQuadro.getBoundingClientRect().left;
+          y = evt.clientY - canvasQuadro.getBoundingClientRect().top;
+          ctx.lineTo(x, y);
+          ctx.stroke();
+        }
+      });
     }
   
-    function queueRenderPage(num) {
-      if (pageRendering) {
-        pageNumPending = num;
-      } else {
-        renderPage(num);
-      }
+    // ----- Main Initialization -----
+    function initialize() {
+      // Configure hand tracking
+      const hands = new Hands({locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+      }});
+  
+      hands.setOptions({
+        selfieMode: true,
+        maxNumHands: 1,
+        modelComplexity: 1,
+        minDetectionConfidence: 0.9,
+        minTrackingConfidence: 0.9
+      });
+  
+      hands.onResults(onResults);
+  
+      // Initialize camera
+      const camera = new Camera(videoElement, {
+        onFrame: async () => {
+          await hands.send({image: videoElement});
+        },
+        width: 1024,
+        height: 768
+      });
+  
+      camera.start();
+  
+      // Set up event listeners
+      setupEventListeners();
     }
   
-    // Função que muda para a página anterior
-    function onPrevPage() {
-      if (pageNum <= 1) {
-        return;
-      }
-      pageNum--;
-      queueRenderPage(pageNum);
-    }
-  
-    // Função que muda para a próxima página
-    function onNextPage() {
-      if (pageNum >= pdfDoc.numPages) {
-        return;
-      }
-      pageNum++;
-      queueRenderPage(pageNum);
-    }
-  
-    // Carrega o slide
-    document.getElementById('inputGroupFile').addEventListener('change', function () {
-      var file = this.files[0];
-      var fileReader = new FileReader();
-      fileReader.onload = function () {
-        var typedarray = new Uint8Array(this.result);
-        pdfjsLib.getDocument(typedarray).promise.then(function (pdfDoc_) {
-          pdfDoc = pdfDoc_;
-          document.getElementById('page_count').textContent = pdfDoc.numPages;
-          renderPage(pageNum);
-        });
-      };
-      fileReader.readAsArrayBuffer(file);
-    });
-  
-    document.getElementById('prev').addEventListener('click', onPrevPage);
-    document.getElementById('next').addEventListener('click', onNextPage);
-  
-    return {
-      renderPage,
-    };
+    // Start the application
+    initialize();
   })();
   
-  // Módulo para o controle de cliques
-  const ClickControlModule = (function () {
-    const botao_prox = document.getElementById("next");
-    const botao_antes = document.getElementById("prev");
-  
-    let canClick = true; // Variável para controlar o delay entre cliques
-  
-    const observer = new MutationObserver((mutations) => {
-      for (const mutation of mutations) {
-        const texto = mutation.target.textContent.toLowerCase();
-        if (texto.includes("direita") && canClick) {
-          PdfDetectionModule.renderPage(PdfDetectionModule.pageNum + 1);
-          canClick = false;
-          setTimeout(() => {
-            canClick = true;
-          }, 5000); // Delay
-          break;
-        } else if (texto.includes("esquerda") && canClick) {
-          PdfDetectionModule.renderPage(PdfDetectionModule.pageNum - 1);
-          canClick = false;
-          setTimeout(() => {
-            canClick = true;
-          }, 5000); // Delay
-          break;
-        }
-      }
-    });
-  
-    observer.observe(document.body, { childList: true, subtree: true });
-  })();  
